f737e6d362f13851f8774011e39c67320a8f04e7
kruzin12 <artur.ferreira@sou.faccat.br>
2025-07-06 20:55:38 -0300

    Update 2_Mapas Interativos.py

--- pages/2_Mapas Interativos.py
+++ pages/2_Mapas Interativos.py
@@ -15,30 +15,30 @@ from utils.processamento import carrega_chegadas
 
 st.set_page_config(page_title="Mapa Coroplético - Chegadas", layout="wide")
 
-# Carrega os dados tratados
+#Carrega os dados tratados
 df = carrega_chegadas()
 
-# Agrega as chegadas por estado
+#Junta chegadas e estado
 df_uf = df.groupby('uf')['chegadas'].sum().reset_index()
 
-# Carrega o GeoJSON com siglas dos estados
+#Carrega o GeoJSON com siglas dos estados
 geojson_url = 'https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson'
 geojson = requests.get(geojson_url).json()
 
-# Mapa coroplético
+#Mapa
 fig = px.choropleth(
     df_uf,
     geojson=geojson,
     locations='uf',
     featureidkey='properties.name',
     color='chegadas',
-    color_continuous_scale='YlOrRd',
+    color_continuous_scale='Viridis',
     title='Chegadas de Turistas por Estado (2024)',
     hover_name='uf',
     labels={'chegadas': 'Chegadas'}
 )
 
-fig.update_traces(marker_line_width=0.8, marker_line_color='black')
+fig.update_traces(marker_line_width=0.5, marker_line_color='black')
 fig.update_geos(fitbounds="locations", visible=False)
 fig.update_layout(margin={"r":0,"t":40,"l":0,"b":0})
 st.plotly_chart(fig, use_container_width=True)
@@ -48,14 +48,14 @@ st.title('Análise Interativa por Estado e Mês')
 
 estado = st.selectbox("Selecione a UF", sorted(df['uf'].unique()))
 
-# Criar dicionário auxiliar para nome dos meses
+#Cria dicionário auxiliar para nome dos meses
 MESES = {
     1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril',
     5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto',
     9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
 }
 
-# Gerar lista de meses únicos ordenada por cod mes
+#Gera lista de meses únicos ordenada por cod mes
 meses_ordenados = (
     df[['cod_mes', 'mes']]
     .drop_duplicates()
@@ -64,10 +64,21 @@ meses_ordenados = (
     .tolist()
 )
 
-# Criar selectbox com ordem correta
+#Cria selectbox com ordem correta
 mes = st.selectbox("Selecione o mês", meses_ordenados)
 
 filtro = df[(df['uf'] == estado) & (df['mes'] == mes) & (df['chegadas'] >= 10)]
 
-fig = px.bar(filtro, x='pais', y='chegadas', color='via', title='Chegadas por País e Via')
+fig = px.bar(
+    filtro,
+    x='pais',
+    y='chegadas',
+    color='via',
+    title='Chegadas por País e Via',
+    labels={
+        'chegadas': 'Chegadas',
+        'pais': 'País',
+        'via': 'Via'
+    }
+)
 st.plotly_chart(fig)
