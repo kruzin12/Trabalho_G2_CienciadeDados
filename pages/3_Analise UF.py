2590fbc9f8395712d636adc755c6830771031fd4
kruzin12 <artur.ferreira@sou.faccat.br>
2025-07-06 20:55:51 -0300

    Update 3_Analise UF.py

--- pages/3_Analise UF.py
+++ pages/3_Analise UF.py
@@ -8,22 +8,23 @@ st.title("Análise por UF")
 
 st.header('Chegadas por País e Via de Entrada')
 
-# Filtro por estado
+#Filtro por estado
 estado = st.selectbox("Selecione o Estado", sorted(df['uf'].unique()))
 df_estado = df[df['uf'] == estado]
 
-# Gráfico de países x via
+#Grafico de paises x via
 fig1, ax1 = plt.subplots(figsize=(10, 6))
 df_pais_via = df_estado.groupby(['pais', 'via'])['chegadas'].sum().reset_index()
 df_pivot = df_pais_via.pivot(index='pais', columns='via', values='chegadas').fillna(0)
-df_pivot = df_pivot[df_pivot.sum(axis=1) > 0].sort_values(by=df_pivot.columns.tolist(), ascending=False).head(10)
+df_pivot = df_pivot[df_pivot.sum(axis=1) > 0].sort_values(by=df_pivot.columns.tolist(), ascending=True).head(10)
+df_pivot.columns.name = 'Via'
 df_pivot.plot(kind='barh', stacked=True, ax=ax1, colormap='tab20')
 ax1.set_title(f'Principais Países de Origem por Via - {estado}')
 ax1.set_xlabel('Chegadas')
 ax1.set_ylabel('País')
 st.pyplot(fig1)
 
-# Gráfico de chegadas por via de transporte
+#Grafico de chegadas por via de transporte
 st.header('Chegadas por Via de Transporte')
 fig2, ax3 = plt.subplots()
 via_data = df.groupby('via')['chegadas'].sum().reset_index()
