464c690b0a832f8fb2d1b4b25fb25b81b8c0ddbf
kruzin12 <artur.ferreira@sou.faccat.br>
2025-07-06 21:12:51 -0300

    Update Descricao dos Dados.py

--- Descricao dos Dados.py
+++ Descricao dos Dados.py
@@ -18,6 +18,30 @@ st.markdown(
     Disponível em: https://dados.gov.br/dados/conjuntos-dados/estimativas-de-chegadas-de-turistas-internacionais-ao-brasil
   """
 )
-
 st.header('Dataframe')
 st.dataframe(carrega_chegadas())
+st.markdown(
+    """
+    ---
+    ## Como Navegar entre as Seções
+
+    O dashboard é dividido em **quatro páginas**:
+
+    - **Resumo Geral**: Apresenta os principais estados em número de chegadas.
+    - **Mapas Interativos**: Permite explorar os dados por UF e mês com filtros dinâmicos, também mostra um mapa interativo por UF.
+    - **Análise UF**: Foca nas características de cada estado individualmente.
+    - **Sazonalidade**: Analisa a variação mensal e a quantidade de chegadas por mês com o mapa de calor.
+
+    ---
+
+    ## Como os Filtros Influenciam os Dados
+
+    As páginas interativas incluem **filtros de UF (estado)** e **mês**, que:
+
+    - **Atualizam os gráficos automaticamente**, refletindo apenas os dados daquele estado ou mês escolhido.
+    - Permitem comparações específicas e análises mais precisas por região e período.
+    - Alguns gráficos também se adaptam ao tipo de **via de entrada (aérea, marítima, terrestre, etc.)**.
+
+    Utilize os filtros para ajustar a visualização conforme o seu interesse e obter insights personalizados.
+    """
+)
